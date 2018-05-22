14689fc68f34580d5f55a51717f93163542fe6b2
Elias5891 <elias5891@gmail.com>
2018-05-22 19:30:44 -0400

    More details on practice, start of setup for the dummy module generation

---
+++ js/puzzles.js
@@ -0,0 +1,12 @@
+function RNG(maxvalue) {
+  return Math.floor(Math.random()*10);
+}
+
+function startEmojiMath() {
+  first_tens = RNG(10);	
+  first_ones = RNG(10);
+  second_tens = RNG(10);	
+  second_ones = RNG(10);
+  
+}
+
