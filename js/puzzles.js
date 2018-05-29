55c836f3e867223e57515a9270d42132f1838ab8
Elias5891 <elias5891@gmail.com>
2018-05-29 19:51:06 -0400

    Got practice working, as well as tweaked the season stuffs

--- js/puzzles.js
+++ js/puzzles.js
@@ -1,5 +1,51 @@
+var first_tens;
+var first_ones;
+var second_tens;
+var second_ones;
+var answer;
+var submittedAnswer;
+
+//Objects to start a few sample modules.
+
 function RNG(maxvalue) {
   return Math.floor(Math.random()*10);
+}
+
+function find_symbol(numeric_value) {
+	switch(numeric_value) {
+		case 0:
+			return ": )";
+			break;
+		case 1:
+			return "= (";		
+			break;
+		case 2:
+			return "( :";
+			break;
+		case 3:
+			return ") =";
+			break;
+		case 4:
+			return ": (";
+			break;
+		case 5:
+			return ") :";
+			break;
+		case 6:
+			return "= )";
+			break;
+		case 7:
+			return "( =";
+			break;
+		case 8:
+			return ": |";
+			break;
+		case 9:
+			return "| :";
+			break;
+		default:
+			break;
+	}
 }
 
 function startEmojiMath() {
@@ -7,6 +53,31 @@ function startEmojiMath() {
   first_ones = RNG(10);
   second_tens = RNG(10);	
   second_ones = RNG(10);
-  
+  var outputMsg = document.getElementById("emoji_puzzle");
+  var symbol_one = find_symbol(first_tens);
+  var symbol_two = find_symbol(first_ones);
+  var symbol_three = find_symbol(second_tens);
+  var symbol_four = find_symbol(second_ones);  
+  answer = (10 * ( first_tens + second_tens) + first_ones + second_ones);
+  outputMsg.innerHTML = "Query:  <br><br>" + symbol_one + " " + symbol_two + " + " + symbol_three + " " + symbol_four;
+  var outputMsg3 = document.getElementById("moduleInstructions");
+  outputMsg3.innerHTML = " Use the following guide to convert emojis to numbers:<br><table><tr><th>Character</th><th>Number</th></tr>                        <tr><td>:)       </td><td>0     </td></tr>                        <tr><td>=(       </td><td>1     </td></tr>                        <tr><td>(:       </td><td>2     </td></tr>                        <tr><td>)=       </td><td>3     </td></tr>                        <tr><td>:(       </td><td>4     </td></tr>                        <tr><td>):       </td><td>5     </td></tr>                        <tr><td>=)       </td><td>6     </td></tr>                        <tr><td>(=       </td><td>7     </td></tr>                        <tr><td>:|       </td><td>8     </td></tr>                        <tr><td>|:       </td><td>9     </td></tr>                    </tbody></table>"; 
+	var outputMsg2 = document.getElementById("correctness");
+	outputMsg2.innerHTML = "";
 }
 
+
+
+
+
+function checkEmojiMath() {
+	console.log("Entered function");
+	var outputMsg2 = document.getElementById("correctness");
+	submittedAnswer = document.getElementById("emoji_answer").value;
+	if (answer == submittedAnswer) {
+		outputMsg2.innerHTML = "Your answer is correct!";
+	} else {
+		outputMsg2.innerHTML = "Your answer (" + submittedAnswer + ") is incorrect.  " + first_tens + first_ones + "+" + second_tens + second_ones + " = " + answer;
+	}
+	
+}
