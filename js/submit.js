b89bd646f67a4afd7306800c00e2532fc61c3e23
Eliott Adkins <elias5891@gmail.com>
2018-07-16 18:34:31 -0400

    Minor fixes/changes
    Repaired function of menu dropdown in small viewports.
    Created popup in case of submission of the contact form.
    Added icon to titlebar for tab viewing.

---
+++ js/submit.js
@@ -0,0 +1,6 @@
+
+
+function submitBroken() {
+  alert("We're sorry, at this time, our form submission is currently not functional.  Please contact us via another method at this time.");
+        return false;
+}
